b50b732c13a4b5b228689cbabfdbb4010daf0bfa
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-12-03 15:32:09 +0900

    chore: add Firebase configuration and initialize in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
-// ignore_for_file: unnecessary_null_comparison, prefer_const_constructors
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:youtube/home_page.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
@@ -13,7 +16,7 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
