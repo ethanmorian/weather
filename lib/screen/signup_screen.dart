ad6792b4ef5e8457a4bab3107393c59bc6bf5460
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-12-05 20:36:50 +0900

    feat: Add SignupScreen for user registration

---
+++ lib/screen/signup_screen.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SignupScreen extends StatefulWidget {
+  final VoidCallback show;
+
+  const SignupScreen(this.show, {super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final email = TextEditingController();
+  FocusNode email_F = FocusNode();
+  final password = TextEditingController();
+  FocusNode password_F = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            SizedBox(
+              width: 96.w,
+              height: 100.h,
+            ),
+            Center(
+              child: Image.asset('assets/logo.jpg'),
+            ),
+            SizedBox(height: 120.h),
+            Textfield(
+              email,
+              Icons.email,
+              'Email',
+              email_F,
+            ),
+            SizedBox(height: 15.h),
+            Textfield(
+              password,
+              Icons.lock,
+              'Password',
+              password_F,
+            ),
+            SizedBox(height: 10.h),
+            Forget(),
+            SizedBox(height: 10.h),
+            Login(),
+            SizedBox(height: 10.h),
+            Have()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget Have() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            "Don't have account?",
+            style: TextStyle(
+              fontSize: 13.sp,
+              color: Colors.grey,
+            ),
+          ),
+          Text(
+            'Signup',
+            style: TextStyle(
+              fontSize: 15.sp,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget Login() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: 44.h,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(10.r),
+        ),
+        child: Text(
+          'Log in',
+          style: TextStyle(
+            fontSize: 23.sp,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget Forget() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      child: Text(
+        'Forget your password?',
+        style: TextStyle(
+          fontSize: 13.sp,
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget Textfield(
+    TextEditingController controller,
+    IconData icon,
+    String type,
+    FocusNode focusNode,
+  ) {
+    return Container(
+      height: 44.h,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.r),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.w),
+        child: TextField(
+          style: TextStyle(
+            fontSize: 18.sp,
+            color: Colors.black,
+          ),
+          controller: controller,
+          focusNode: focusNode,
+          decoration: InputDecoration(
+            hintText: type,
+            prefixIcon: Icon(
+              icon,
+              color: focusNode.hasFocus ? Colors.black : Colors.grey,
+            ),
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 15.w,
+              vertical: 15.h,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5.r),
+              borderSide: BorderSide(
+                color: Colors.grey,
+                width: 2.w,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5.r),
+              borderSide: BorderSide(
+                color: Colors.black,
+                width: 2.w,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
