502ce72e9a315a62829d394be87abc322961ec2f
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-12-10 18:01:48 +0900

    feat: add Navigation widget with bottom navigation and page view

---
+++ lib/widget/navigation.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+int _currentIndex = 0;
+
+class _NavigationState extends State<Navigation> {
+  late PageController pageController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    pageController.dispose();
+  }
+
+  onPageChanged(int page) {
+    setState(() {
+      _currentIndex = page;
+    });
+  }
+
+  navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          currentIndex: _currentIndex,
+          onTap: navigationTapped,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.camera),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: '',
+            ),
+          ],
+        ),
+      ),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        children: [],
+      ),
+    );
+  }
+}
