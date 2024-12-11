fddbe130449b6beb0936242ffbc436c94553f292
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-12-11 19:26:35 +0900

    feat: Implement media fetching and display for adding new posts

--- lib/screen/add_post.dart
+++ lib/screen/add_post.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class AddPost extends StatefulWidget {
   const AddPost({super.key});
@@ -8,11 +12,156 @@ class AddPost extends StatefulWidget {
 }
 
 class _AddPostState extends State<AddPost> {
+  final List<Widget> _mediaList = [];
+  final List<File> path = [];
+  File? _file;
+  int currentPage = 0;
+  int? lastPage;
+
+  _fetchNewMedia() async {
+    lastPage = currentPage;
+    final PermissionState ps = await PhotoManager.requestPermissionExtend();
+    if (ps.isAuth) {
+      List<AssetPathEntity> album =
+          await PhotoManager.getAssetPathList(onlyAll: true);
+      List<AssetEntity> media = await album[0].getAssetListPaged(
+        page: currentPage,
+        size: 60,
+      );
+      for (var asset in media) {
+        if (asset.type == AssetType.image) {
+          final file = await asset.file;
+          if (file != null) {
+            path.add(
+              File(file.path),
+            );
+            _file = path[0];
+          }
+        }
+      }
+      List<Widget> temp = [];
+      for (var asset in media) {
+        temp.add(
+          FutureBuilder(
+            future: asset.thumbnailDataWithSize(
+              const ThumbnailSize(
+                200,
+                200,
+              ),
+            ),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Container(
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Image.memory(
+                          snapshot.data!,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }
+              return Container();
+            },
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchNewMedia();
+  }
+
+  int indexx = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('add post'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          'New Post',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: false,
+        actions: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.w),
+            child: Text(
+              'Next',
+              style: TextStyle(
+                fontSize: 15.sp,
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 375.h,
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1,
+                            mainAxisSpacing: 1,
+                            crossAxisSpacing: 1),
+                    itemBuilder: (context, index) {
+                      return _mediaList[indexx];
+                    },
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 40.h,
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      SizedBox(width: 10.w),
+                      Text(
+                        'Recent',
+                        style: TextStyle(
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: _mediaList.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 1,
+                      crossAxisSpacing: 1),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          indexx = index;
+                        });
+                      },
+                      child: _mediaList[index],
+                    );
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
