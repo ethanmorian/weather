26a421e2e899e6bc67f039e1fcf9e714f614cfab
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-12-14 18:29:54 +0900

    feat: Implement weather UI with hourly and daily forecasts

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.weather
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,13 +11,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -36,7 +44,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            WeatherScreen()
         }
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        window.decorView.systemUiVisibility =
+            window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
     }
 }
 
@@ -50,7 +67,7 @@ fun WeatherScreen() {
                 brush = Brush.horizontalGradient(
                     colors = listOf(
                         Color(android.graphics.Color.parseColor("#59469d")),
-                        Color(android.graphics.Color.parseColor("#643d67")),
+                        Color(android.graphics.Color.parseColor("#643d67"))
                     )
                 )
             )
@@ -59,7 +76,7 @@ fun WeatherScreen() {
         Column(modifier = Modifier.fillMaxSize()) {
             LazyColumn(
                 modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 item {
                     Text(
@@ -69,7 +86,7 @@ fun WeatherScreen() {
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(top = 48.dp),
-                        textAlign = TextAlign.Center,
+                        textAlign = TextAlign.Center
                     )
 
                     Image(
@@ -77,7 +94,7 @@ fun WeatherScreen() {
                         contentDescription = null,
                         modifier = Modifier
                             .size(150.dp)
-                            .padding(top = 8.dp),
+                            .padding(top = 8.dp)
                     )
 
                     // Display date and time
@@ -88,7 +105,7 @@ fun WeatherScreen() {
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(top = 8.dp),
-                        textAlign = TextAlign.Center,
+                        textAlign = TextAlign.Center
                     )
 
                     // Display temperature details
@@ -100,7 +117,7 @@ fun WeatherScreen() {
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(top = 8.dp),
-                        textAlign = TextAlign.Center,
+                        textAlign = TextAlign.Center
                     )
 
                     Text(
@@ -110,7 +127,7 @@ fun WeatherScreen() {
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(top = 8.dp),
-                        textAlign = TextAlign.Center,
+                        textAlign = TextAlign.Center
                     )
 
                     // Box containing weather details like rain, wind speed, humidity
@@ -120,7 +137,7 @@ fun WeatherScreen() {
                             .padding(horizontal = 24.dp, vertical = 16.dp)
                             .background(
                                 color = colorResource(id = R.color.purple),
-                                shape = RoundedCornerShape(25.dp),
+                                shape = RoundedCornerShape(25.dp)
                             )
                     ) {
                         Row(
@@ -129,11 +146,245 @@ fun WeatherScreen() {
                                 .height(100.dp)
                                 .padding(horizontal = 8.dp),
                             verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                        ) {}
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            WeatherDetailItem(
+                                icon = R.drawable.rain,
+                                value = "22%",
+                                label = "Rain"
+                            )
+
+                            WeatherDetailItem(
+                                icon = R.drawable.wind,
+                                value = "12 km/h",
+                                label = "Wind Speed"
+                            )
+
+                            WeatherDetailItem(
+                                icon = R.drawable.humidity,
+                                value = "18%",
+                                label = "Humidity"
+                            )
+                        }
                     }
+
+                    // Displaying "Today" label
+                    Text(
+                        text = "Today",
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                horizontal = 24.dp, vertical = 8.dp
+                            )
+                    )
+                }
+
+                // Display future hourly forecast using a LazyRow
+                item {
+                    LazyRow(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentPadding = PaddingValues(horizontal = 20.dp),
+                        horizontalArrangement = Arrangement.spacedBy(4.dp)
+                    ) {
+                        items(items) { item ->
+                            FutureModelViewHolder(item)
+                        }
+                    }
+                }
+
+                // Display "Future" label and next 7 days button
+                item {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 24.dp, vertical = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Future",
+                            fontSize = 20.sp,
+                            color = Color.White,
+                            modifier = Modifier.weight(1f)
+                        )
+
+                        Text(
+                            text = "Next 7 day >",
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+                items(dailyItems) {
+                    FutureItem(item = it)
                 }
             }
         }
+    }
+}
+
+// Display each future daily forecast item
+@Composable
+fun FutureItem(item: FutureModel) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp, vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = item.day,
+            color = Color.White,
+            fontSize = 14.sp
+        )
+
+        Image(
+            painter = painterResource(
+                id = getDrawableResourceId(picPath = item.picPath)
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(start = 32.dp)
+                .size(45.dp)
+        )
+
+        Text(
+            text = item.status,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            color = Color.White,
+            fontSize = 14.sp
+        )
+
+        Text(
+            text = "${item.highTemp}º",
+            modifier = Modifier
+                .padding(end = 16.dp),
+            color = Color.White,
+            fontSize = 14.sp
+        )
+
+        Text(
+            text = "${item.lowTemp}º",
+            color = Color.White,
+            fontSize = 14.sp
+        )
+    }
+}
+
+@Composable
+fun getDrawableResourceId(picPath: String): Int {
+    return when (picPath) {
+        "storm" -> R.drawable.storm
+        "cloudy" -> R.drawable.cloudy
+        "windy" -> R.drawable.windy
+        "cloudy_sunny" -> R.drawable.cloudy_sunny
+        "sunny" -> R.drawable.sunny
+        "rainy" -> R.drawable.rainy
+        else -> R.drawable.sunny
+    }
+}
+
+// sample daily data
+val dailyItems = listOf(
+    FutureModel("Sat", "storm", "Storm", 24, 12),
+    FutureModel("Sun", "cloudy", "Cloudy", 25, 16),
+    FutureModel("Mon", "windy", "Windy", 29, 15),
+    FutureModel("Tue", "cloudy_sunny", "Cloudy Sunny", 23, 15),
+    FutureModel("Wed", "sunny", "Sunny", 28, 11),
+    FutureModel("Thu", "rainy", "Rainy", 23, 12)
+)
+
+// ViewHolder for each hourly forecast item
+@Composable
+fun FutureModelViewHolder(model: HourlyModel) {
+    Column(
+        modifier = Modifier
+            .width(90.dp)
+            .wrapContentHeight()
+            .padding(4.dp)
+            .background(
+                color = colorResource(id = R.color.purple),
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = model.hour,
+            color = Color.White,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            textAlign = TextAlign.Center
+        )
+
+        Image(
+            painter = painterResource(
+                id = when (model.picPath) {
+                    "cloudy" -> R.drawable.cloudy
+                    "sunny" -> R.drawable.sunny
+                    "wind" -> R.drawable.wind
+                    "rainy" -> R.drawable.rainy
+                    "storm" -> R.drawable.storm
+                    else -> R.drawable.sunny
+                }
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .size(45.dp)
+                .padding(8.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = "${model.temp}",
+            color = Color.White,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+// sample hourly data
+val items = listOf(
+    HourlyModel("9 pm", 28, "cloudy"),
+    HourlyModel("10 pm", 28, "sunny"),
+    HourlyModel("11 pm", 28, "wind"),
+    HourlyModel("12 pm", 28, "rainy"),
+    HourlyModel("1 am", 28, "storm")
+)
+
+@Composable
+fun WeatherDetailItem(icon: Int, value: String, label: String) {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            modifier = Modifier.size(34.dp)
+        )
+
+        Text(
+            text = value,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.white),
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = label,
+            color = colorResource(id = R.color.white),
+            textAlign = TextAlign.Center
+        )
     }
 }
